0d1bc57f89d5fc22a22401d7108f50bb5ee23b07
Zeker2908 <mud.runner@bk.ru>
2024-04-25 19:04:00 +0400

    init

---
+++ classified/src/main/java/ru/lebruce/classified/ClassifiedApplication.java
@@ -0,0 +1,13 @@
+package ru.lebruce.classified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassifiedApplication.class, args);
+	}
+
+}
---
+++ classified/src/test/java/ru/lebruce/classified/ClassifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebruce.classified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
