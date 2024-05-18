1412b5553970987e79e9ece7d21705e3ce771766
Zeker2908 <mud.runner@bk.ru>
2024-05-18 08:06:04 +0400

    Update

--- store/src/main/java/ru/lebruce/store/service/impl/EmailServiceImpl.java
+++ store/src/main/java/ru/lebruce/store/service/impl/EmailServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -29,6 +30,7 @@ public class EmailServiceImpl implements EmailService {
     @Value("${app.url}")
     private String appUrl;
 
+    @Async("taskExecutor")
     @Override
     public void sendConfirmationEmail(EmailContext email) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
