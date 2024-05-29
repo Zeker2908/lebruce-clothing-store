6e9083dd25ddc5e7aa09c540b0dbb8759e180c0b
Zeker2908 <mud.runner@bk.ru>
2024-05-29 14:35:05 +0400

    Update

--- store/src/main/java/ru/lebruce/store/domain/dto/ReviewRequest.java
+++ store/src/main/java/ru/lebruce/store/domain/dto/ReviewRequest.java
@@ -12,9 +12,6 @@ public class ReviewRequest {
     @NotNull(message = "ProductId не должно быть пустым")
     private Long productId;
 
-    @NotNull(message = "UserId не должно быть пустым")
-    private Long userId;
-
     @NotNull(message = "Rating не должно быть пустым")
     @Range(min = 1, max = 5, message = "Rating должен быть от 1 до 5")
     private Integer rating;
